4bae0172dcad6555ec54ef163d953510344b013b
mohanson <mohanson@outlook.com>
2024-08-01 15:01:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CKB Easy
+
+CKB Easy is an experimental project that aims to provide human-friendly interfaces for common CKB operations. Note that CKB Easy is not a complete SDK, but only implements the CKB functions that I am interested in.
+
+Features:
+
+- No third-party dependencies. All code is visible.
+- Incredibly simple.
+
+```toml
+[dependencies]
+ckb-ez = "0.1.0"
+```
---
+++ src/core.rs
@@ -0,0 +1,301 @@
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Script {
+    pub code_hash: [u8; 32],
+    pub hash_type: u8,
+    pub args: Vec<u8>,
+}
+
+impl Script {
+    pub fn new(code_hash: [u8; 32], hash_type: u8, args: Vec<u8>) -> Self {
+        Self { code_hash, hash_type, args }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_dynvec(vec![
+            crate::molecule::Byte32::new(self.code_hash).molecule(),
+            crate::molecule::Byte::new(self.hash_type).molecule(),
+            crate::molecule::Bytes::new(self.args.clone()).molecule(),
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result = crate::molecule::decode_dynvec(data);
+        Self {
+            code_hash: crate::molecule::Byte32::molecule_decode(&result[0]),
+            hash_type: crate::molecule::Byte::molecule_decode(&result[1]),
+            args: crate::molecule::Bytes::molecule_decode(&result[2]),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct OutPoint {
+    pub tx_hash: [u8; 32],
+    pub index: u32,
+}
+
+impl OutPoint {
+    pub fn new(tx_hash: [u8; 32], index: u32) -> Self {
+        Self { tx_hash, index }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_seq(vec![
+            crate::molecule::Byte32::new(self.tx_hash).molecule(),
+            crate::molecule::U32::new(self.index).molecule(),
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result = crate::molecule::decode_seq(
+            data,
+            &[crate::molecule::Byte32::molecule_size(), crate::molecule::U32::molecule_size()],
+        );
+        Self {
+            tx_hash: crate::molecule::Byte32::molecule_decode(&result[0]),
+            index: crate::molecule::U32::molecule_decode(&result[1]),
+        }
+    }
+
+    pub fn molecule_size() -> usize {
+        crate::molecule::Byte32::molecule_size() + crate::molecule::U32::molecule_size()
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct CellInput {
+    since: u64,
+    previous_output: OutPoint,
+}
+
+impl CellInput {
+    pub fn new(since: u64, previous_output: OutPoint) -> Self {
+        Self { since, previous_output }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_seq(vec![
+            crate::molecule::U64::new(self.since).molecule(),
+            self.previous_output.molecule(),
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result =
+            crate::molecule::decode_seq(data, &[crate::molecule::U64::molecule_size(), OutPoint::molecule_size()]);
+        CellInput {
+            since: crate::molecule::U64::molecule_decode(&result[0]),
+            previous_output: OutPoint::molecule_decode(&result[1]),
+        }
+    }
+
+    pub fn molecule_size() -> usize {
+        crate::molecule::U64::molecule_size() + OutPoint::molecule_size()
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct CellOutput {
+    capacity: u64,
+    lock: Script,
+    kype: Option<Script>,
+}
+
+impl CellOutput {
+    pub fn new(capacity: u64, lock: Script, kype: Option<Script>) -> Self {
+        Self { capacity, lock, kype }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_dynvec(vec![
+            crate::molecule::U64::new(self.capacity).molecule(),
+            self.lock.molecule(),
+            match &self.kype {
+                Some(kype) => kype.molecule(),
+                None => vec![],
+            },
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result = crate::molecule::decode_dynvec(data);
+        CellOutput {
+            capacity: crate::molecule::U64::molecule_decode(&result[0]),
+            lock: Script::molecule_decode(&result[1]),
+            kype: if !result[2].is_empty() { Some(Script::molecule_decode(&result[2])) } else { None },
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct CellDep {
+    out_point: OutPoint,
+    dep_type: u8,
+}
+
+impl CellDep {
+    pub fn new(out_point: OutPoint, dep_type: u8) -> Self {
+        Self { out_point, dep_type }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_seq(vec![
+            self.out_point.molecule(),
+            crate::molecule::Byte::new(self.dep_type).molecule(),
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result =
+            crate::molecule::decode_seq(data, &[OutPoint::molecule_size(), crate::molecule::Byte::molecule_size()]);
+        CellDep {
+            out_point: OutPoint::molecule_decode(&result[0]),
+            dep_type: crate::molecule::Byte::molecule_decode(&result[1]),
+        }
+    }
+
+    pub fn molecule_size() -> usize {
+        OutPoint::molecule_size() + crate::molecule::Byte::molecule_size()
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TransactionRaw {
+    version: u32,
+    cell_deps: Vec<CellDep>,
+    header_deps: Vec<[u8; 32]>,
+    inputs: Vec<CellInput>,
+    outputs: Vec<CellOutput>,
+    outputs_data: Vec<Vec<u8>>,
+}
+
+impl TransactionRaw {
+    pub fn new(
+        version: u32,
+        cell_deps: Vec<CellDep>,
+        header_deps: Vec<[u8; 32]>,
+        inputs: Vec<CellInput>,
+        outputs: Vec<CellOutput>,
+        outputs_data: Vec<Vec<u8>>,
+    ) -> Self {
+        Self { version, cell_deps, header_deps, inputs, outputs, outputs_data }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_dynvec(vec![
+            crate::molecule::U32::new(self.version).molecule(),
+            crate::molecule::encode_fixvec(self.cell_deps.iter().map(|e| e.molecule()).collect()),
+            crate::molecule::encode_fixvec(
+                self.header_deps.iter().map(|e| crate::molecule::Byte32::new(*e).molecule()).collect(),
+            ),
+            crate::molecule::encode_fixvec(self.inputs.iter().map(|e| e.molecule()).collect()),
+            crate::molecule::encode_dynvec(self.outputs.iter().map(|e| e.molecule()).collect()),
+            crate::molecule::encode_dynvec(
+                self.outputs_data.iter().map(|e| crate::molecule::Bytes::new(e.clone()).molecule()).collect(),
+            ),
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result = crate::molecule::decode_dynvec(data);
+        Self {
+            version: crate::molecule::U32::molecule_decode(&result[0]),
+            cell_deps: crate::molecule::decode_fixvec(&result[1])
+                .iter()
+                .map(|e| CellDep::molecule_decode(&e))
+                .collect(),
+            header_deps: crate::molecule::decode_fixvec(&result[2])
+                .iter()
+                .map(|e| crate::molecule::Byte32::molecule_decode(&e))
+                .collect(),
+            inputs: crate::molecule::decode_fixvec(&result[3]).iter().map(|e| CellInput::molecule_decode(&e)).collect(),
+            outputs: crate::molecule::decode_dynvec(&result[4])
+                .iter()
+                .map(|e| CellOutput::molecule_decode(&e))
+                .collect(),
+            outputs_data: crate::molecule::decode_dynvec(&result[5])
+                .iter()
+                .map(|e| crate::molecule::Bytes::molecule_decode(&e))
+                .collect(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Transaction {
+    raw: TransactionRaw,
+    witnesses: Vec<Vec<u8>>,
+}
+
+impl Transaction {
+    pub fn new(raw: TransactionRaw, witnesses: Vec<Vec<u8>>) -> Self {
+        Self { raw, witnesses }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_dynvec(vec![
+            self.raw.molecule(),
+            crate::molecule::encode_dynvec(
+                self.witnesses.iter().map(|e| crate::molecule::Bytes::new(e.clone()).molecule()).collect(),
+            ),
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result = crate::molecule::decode_dynvec(data);
+        Transaction {
+            raw: TransactionRaw::molecule_decode(&result[0]),
+            witnesses: crate::molecule::decode_dynvec(&result[1])
+                .iter()
+                .map(|e| crate::molecule::Bytes::molecule_decode(&e))
+                .collect(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct WitnessArgs {
+    lock: Option<Vec<u8>>,
+    input_type: Option<Vec<u8>>,
+    output_type: Option<Vec<u8>>,
+}
+
+impl WitnessArgs {
+    pub fn new(lock: Option<Vec<u8>>, input_type: Option<Vec<u8>>, output_type: Option<Vec<u8>>) -> Self {
+        Self { lock, input_type, output_type }
+    }
+
+    pub fn molecule(&self) -> Vec<u8> {
+        crate::molecule::encode_dynvec(vec![
+            match &self.lock {
+                Some(lock) => crate::molecule::Bytes::new(lock.clone()).molecule(),
+                None => vec![],
+            },
+            match &self.input_type {
+                Some(kype) => crate::molecule::Bytes::new(kype.clone()).molecule(),
+                None => vec![],
+            },
+            match &self.output_type {
+                Some(kype) => crate::molecule::Bytes::new(kype.clone()).molecule(),
+                None => vec![],
+            },
+        ])
+    }
+
+    pub fn molecule_decode(data: &[u8]) -> Self {
+        let result = crate::molecule::decode_dynvec(data);
+        Self {
+            lock: if !result[0].is_empty() { Some(crate::molecule::Bytes::molecule_decode(&result[0])) } else { None },
+            input_type: if !result[1].is_empty() {
+                Some(crate::molecule::Bytes::molecule_decode(&result[1]))
+            } else {
+                None
+            },
+            output_type: if !result[2].is_empty() {
+                Some(crate::molecule::Bytes::molecule_decode(&result[2]))
+            } else {
+                None
+            },
+        }
+    }
+}
